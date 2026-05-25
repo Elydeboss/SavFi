419d5476fc8b2baf67dbde27279f41910f4830d3
elijah <tomelijah349@gmail.com>
2026-05-25 16:33:49 +0100

    reslove 405 error

--- Frontend/src/config/api.ts
+++ Frontend/src/config/api.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://wallet-api-55mt.onrender.com";
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "https://savfi-v2-backend.onrender.com";
